b5f7b6e088d925a4b2f386017979e2ca64420093
Marcel Schoffelmeer <marcel@bluecore.com>
2020-04-18 14:22:44 -0400

    Initial Commit

---
+++ test4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test4
+//
+//  Created by Marcel on 4/18/20.
+//  Copyright © 2020 Marcel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
